6549164320c85849434a207aab2630a8c8320ea1
Emily Jirles <emily.jirles@gmail.com>
2018-07-27 04:01:35 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export class FillerText extends Component {
+export default class FillerText extends Component {
     render(){
         return <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
     }
